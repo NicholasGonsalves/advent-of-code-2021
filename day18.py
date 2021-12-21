9036ecab350b3a80eef560e5f3de95d29b676a2b
nick <gonzo.nick123@gmail.com>
2021-12-21 00:37:11 +0000

    comments

--- day18.py
+++ day18.py
@@ -46,24 +46,25 @@ class Number:
         return self + other
 
     def load_tree(self) -> None:
+        """Load tree from list representation, including parent node pointers."""
         self.root = self.load_node(self.root, self.number)
         self.parent_fix(self.root)
 
     def load_node(self, root, leaf):
+        """Assign specific nodes a place in the tree recursively."""
         if root is None:
             root = Node()
 
         if isinstance(leaf, int):
             return Node(value=leaf)
 
-        # if isinstance(leaf, list):
         left, right = leaf
         root.l = self.load_node(Node(), left)
         root.r = self.load_node(Node(), right)
         return root
 
-    # Fixes parent pointers for already defined tree
     def parent_fix(self, node):
+        """Fix parent pointers in tree, as initial tree construction does not handle them correctly."""
         while node.l is not None and node.l.p is None:
             node.l.p = node
             self.parent_fix(node.l)
@@ -72,7 +73,7 @@ class Number:
             self.parent_fix(node.r)
 
     def find_exploder(self, root, depth=0):
-        # find leftmost pair nested 4 deep
+        """Find first (left-most using dfs) node that meets criteria for exploding (is nested >4 deep)."""
         if root:
             exploder = self.find_exploder(root.l, depth + 1)
             if exploder: return exploder
@@ -85,6 +86,7 @@ class Number:
         return None
 
     def get_left_number(self, node):
+        """Get first node with a non-None value to the 'left' of the passed node."""
         if node.p is None: return None
         if node == node.p.l:
             return self.get_left_number(node.p)
@@ -95,6 +97,7 @@ class Number:
         return n
 
     def get_right_number(self, node):
+        """Get first node with a non-None value to the 'right' of the passed node."""
         if node.p is None: return None
         if node == node.p.r:
             return self.get_right_number(node.p)
@@ -105,17 +108,22 @@ class Number:
         return n
 
     def explode(self):
+        """Apply explosion to found exploder node."""
+        # get node to 'explode'
         exploder = self.find_exploder(self.root)
         if exploder is None: return False
 
+        # add left val of exploding node to first left val node
         next_l = self.get_left_number(exploder.l)
         if next_l is not None:
             next_l.v += exploder.l.v
 
+        # add right val of exploding node to first right val node
         next_r = self.get_right_number(exploder.r)
         if next_r is not None:
             next_r.v += exploder.r.v
 
+        # exploding node val set to 0, and is now leaf
         exploder.l = None
         exploder.r = None
         exploder.v = 0
@@ -123,15 +131,17 @@ class Number:
         return True
 
     def update_number_from_tree(self):
+        """Condense tree (root) into list representation of number and assign to self."""
         self.number = ast.literal_eval(self.get_number_string_from_tree(self.root))
 
     def get_number_string_from_tree(self, node):
+        """Get string representation of the passed tree/node recursively."""
         if node.v is not None:
             return str(node.v)
         return '[' + self.get_number_string_from_tree(node.l) + ',' + self.get_number_string_from_tree(node.r) + ']'
 
     def find_to_split(self, root):
-        # find first value greater than or equal to 10
+        """Find first node with a value greater than or equal to 10. DFS."""
         if root:
             split = self.find_to_split(root.l)
             if split: return split
@@ -142,6 +152,7 @@ class Number:
         return None
 
     def split(self):
+        """Split value according to the rules, and assigned respective nodes."""
         to_split = self.find_to_split(self.root)
         if to_split is None: return False
 
@@ -158,15 +169,18 @@ class Number:
         return True
 
     def get_magnitude(self):
+        """Get magnitude of current self."""
         return self.magnitude(self.root)
 
     def magnitude(self, root):
+        """Get magnitude of passed tree/node representation of the number."""
         if root.v is not None:
             return root.v
         return 3 * self.magnitude(root.l) + 2 * self.magnitude(root.r)
 
 
 def part_one(homework):
+    # Sum all Numbers in homework and return magnitude of result.
     numbers = [Number(number) for number in homework]
     answer = sum(numbers[1:], start=numbers[0])
     magnitude = answer.get_magnitude()
@@ -174,6 +188,7 @@ def part_one(homework):
 
 
 def update_max_magnitude_with_pair(a, b, max_magnitude):
+    """Update and return max_magnitude for the passed pair of numbers (helper for part 2)."""
     answer = a + b
     magnitude = answer.get_magnitude()
     if magnitude > max_magnitude:
@@ -182,7 +197,7 @@ def update_max_magnitude_with_pair(a, b, max_magnitude):
 
 
 def part_two(homework):
-    # just try all combinations
+    # Return max magnitude of the sum of any pair of values in the homework
     max_magnitude = 0
     numbers = [Number(number) for number in homework]
     pairs = combinations(numbers, 2)
