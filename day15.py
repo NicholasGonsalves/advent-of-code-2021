ede723b06f85fd826ec81e5ec80497a7c3a7f64b
nick <gonzo.nick123@gmail.com>
2021-12-21 00:12:46 +0000

    dijkstra now with heapq, fast, but value off slightly on some inputs?

--- day15.py
+++ day15.py
@@ -1,6 +1,6 @@
 """Day 15."""
 from copy import deepcopy
-from heapq import heappush, heappop
+from heapq import heapify, heappush, heappop
 
 
 def read_file(file_name: str):
@@ -13,34 +13,34 @@ def read_file(file_name: str):
 
 def find_path(grid):
     # Dijkstra (wikipedia)
-    queue, visited, dist = [], {}, {}
+    queue = []
+    heapify(queue)
+    heappush(queue, (0, (0, 0)))
+
+    visited, dist = {}, {}
+    dist[(0, 0)] = 0
+    visited[(0, 0)] = 0
+
     for i in range(len(grid)):
         for j in range(len(grid[0])):
-            dist[(i, j)] = float('inf')
-            queue.append((i, j))
-            visited[(i, j)] = None
-    dist[(0, 0)] = 0
+            if not ((i == 0) and (j == 0)):
+                dist[(i, j)] = float('inf')
+                heappush(queue, (float('inf'), (i, j)))
 
     def in_bounds(point):
         if (point[0] >= 0) and (point[0] < len(grid)) and (point[1] >= 0) and (point[1] < len(grid[0])):
             return True
         return False
 
-    checked_nodes = 0
     while queue:
-        checked_nodes += 1
-        u = min(queue, key=dist.__getitem__)
-        queue.remove(u)
+        _, u = heappop(queue)
 
-        for neighbour in [(u[0]+1, u[1]), (u[0], u[1]+1)]:
+        for neighbour in [(u[0]+1, u[1]), (u[0]-1, u[1]), (u[0], u[1]+1), (u[0], u[1]-1)]:
             if not in_bounds(neighbour): continue
             alt = dist[u] + grid[neighbour[0]][neighbour[1]]
             if alt < dist[neighbour]:
                 dist[neighbour] = alt
                 visited[neighbour] = u
-
-        if checked_nodes % 100 == 0:
-            print(f"Checked {checked_nodes} nodes.")
 
     # Step backwards through values to get path
     destination = (len(grid)-1, len(grid[0])-1)
@@ -51,12 +51,12 @@ def find_path(grid):
         path.append(visited[current])
         current = visited[current]
 
-    return list(reversed(path))
+    return path
 
 
 def part_one(cave):
     path = find_path(cave)
-    return sum([cave[p[0]][p[1]] for p in path]) - cave[0][0]
+    return sum([cave[p[0]][p[1]] for p in path[:-1]])
 
 
 def apply_wrapping_increment(tile, increment):
@@ -107,8 +107,8 @@ def part_two(cave):
 
     # Compute path
     path = find_path(expanded_cave)
-    return sum([expanded_cave[p[0]][p[1]] for p in path]) - expanded_cave[0][0]
-
+    # todo error in return val... off by small amount?
+    return sum([expanded_cave[p[0]][p[1]] for p in path[:-1]])
 
 
 if __name__ == "__main__":
